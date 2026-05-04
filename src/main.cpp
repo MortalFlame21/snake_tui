1491a41aba86a61b2359732155170cbf0975e3ed
MortalFlame21 <markgio2100@gmail.com>
2026-05-04 13:06:13 +1000

    feat: main.cpp. incrementing score.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
 #include <format>
+#include <thread>
 
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/loop.hpp>
 
 #include "Game.h"
+
+constexpr int g_FPS{30};
 
 ftxui::Element cell(std::string s) { return ftxui::text(s) | ftxui::border; }
 
@@ -15,16 +19,17 @@ int main() {
     Game game{};
 
     auto screen{ftxui::ScreenInteractive::TerminalOutput()};
-    auto contents{ftxui::vbox({
-        ftxui::hbox({
-            cell("Snake TUI"),
-            ftxui::filler(),
-            cell(std::format("Score: {}", game.score()))
-        }),
-        ftxui::canvas(game.toCanvas()) | ftxui::center,
-        ftxui::separatorEmpty()
-    }) | ftxui::hcenter};
-    auto renderer{ftxui::Renderer([&]() { return contents; })};
+    auto renderer{ftxui::Renderer([&]() {
+        return ftxui::vbox({
+            ftxui::hbox({
+                cell("Snake TUI"),
+                ftxui::filler(),
+                cell(std::format("Score: {}", game.score()))
+            }),
+            ftxui::canvas(game.toCanvas()) | ftxui::center,
+            ftxui::separatorEmpty()
+        }) | ftxui::hcenter;
+    })};
     auto component{ftxui::CatchEvent(renderer, [&](ftxui::Event event) {
         if (event == ftxui::Event::Character('q')) {
             screen.ExitLoopClosure()();
@@ -33,5 +38,13 @@ int main() {
         return false;
     })};
 
-    screen.Loop(component);
+    ftxui::Loop loop(&screen, component);
+
+    while (!loop.HasQuitted()) {
+        game.snake_move();
+        ++game.score();
+        loop.RunOnce();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000) / g_FPS);
+        screen.RequestAnimationFrame();
+    }
 }
