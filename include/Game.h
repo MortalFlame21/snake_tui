9dccceffa45f91a3ccf0423d216b7805767c38bf
MortalFlame21 <markgio2100@gmail.com>
2026-05-09 00:05:44 +1000

    feat: Game. Move logic to Game.

--- include/Game.h
+++ include/Game.h
@@ -10,15 +10,18 @@ class Game {
 public:
     inline static constexpr size_t cell_sz{10};
 
-    Game();
+    Game() = default;
 
-    int& score() { return score_; }; // temp
+    void run();
     int score() const;
     bool hasWon() const;
+    bool hasLost() const;
     ftxui::Canvas toCanvas() const;
     void snake_move();
     void snake_turn(Snake::Facing turn);
 private:
+    inline static constexpr size_t FPS{30};
+
     Grid grid_{};
     Snake snake_{};
     int score_{};
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,62 @@
 #include "Game.h"
 
-#include <ftxui/dom/canvas.hpp>
+#include <iostream>
+#include <string>
+#include <format>
+#include <thread>
 
-Game::Game() { }
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/canvas.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/loop.hpp>
+
+// helper
+
+ftxui::Element cell(std::string s) { return ftxui::text(s) | ftxui::border; }
+
+void Game::run() {
+    auto screen{ftxui::ScreenInteractive::TerminalOutput()};
+
+    auto renderer{ftxui::Renderer([&]() {
+        return ftxui::vbox({
+            ftxui::hbox({
+                cell("Snake TUI"),
+                ftxui::filler(),
+                cell(std::format("Score: {}", score()))
+            }),
+            ftxui::canvas(toCanvas()) | ftxui::center,
+            ftxui::separatorEmpty()
+        }) | ftxui::hcenter;
+    })};
+
+    auto component{ftxui::CatchEvent(renderer, [&](ftxui::Event event) {
+        if (event == ftxui::Event::Character('q')) {
+            screen.ExitLoopClosure()();
+            return true;
+        }
+        return false;
+    })};
+
+    ftxui::Loop loop(&screen, component);
+
+    while (!loop.HasQuitted()) {
+        snake_move();
+
+        loop.RunOnce();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000) / FPS);
+        screen.RequestAnimationFrame();
+
+        if (hasLost())
+            break;
+    }
+}
+
 
 bool Game::hasWon() const { return score_ >= grid_.tiles(); }
+
+bool Game::hasLost() const { return !snake_.inGrid(grid_); }
 
 int Game::score() const { return score_; }
 
@@ -17,10 +69,18 @@ ftxui::Canvas Game::toCanvas() const {
     grid_.toCanvas(cva);
     snake_.toCanvas(cva);
 
+    if (hasLost()) {
+        auto msg{std::format("You lose! Score: {}.", score_)};
+
+        cva.DrawText(cva_w / 2 - msg.length(), cva_h / 2, msg);
+    }
+
     return cva;
 }
 
-void Game::snake_move() { snake_.move(); }
+void Game::snake_move() {
+    snake_.move();
+}
 
 void Game::snake_turn(Snake::Facing turn) {
     // implement soon!
