6fb267032c39b212b0ae1e2d4ee8d85c6a844648
MortalFlame21 <markgio2100@gmail.com>
2026-05-11 00:54:16 +1000

    update: Grid. header only.

--- include/Grid.h
+++ include/Grid.h
@@ -15,7 +15,22 @@ public:
     static constexpr int cols() { return cols_; }
     static constexpr int tiles() { return rows_ * cols_; };
 
-    ftxui::Canvas toCanvas(ftxui::Canvas& cva) const;
+    void toCanvas(ftxui::Canvas& cva) const {
+        // output a 10 x 10 square, or 4 x 2 printed blocks
+        for (size_t r{}; r < rows_; ++r) {
+            for (size_t c{}; c < cols_; ++c) {
+                auto light{(r + c) % 2 == 0};
+                if (!light) continue;
+
+                // fill square at r, c position
+                for (size_t dy{}; dy < cell_sz; ++dy) {
+                    for (size_t dx{}; dx < cell_sz; ++dx) {
+                        cva.DrawBlockOn(c * cell_sz + dx, r * cell_sz + dy);
+                    }
+                }
+            }
+        }
+    }
 private:
     inline static constexpr int rows_{10};
     inline static constexpr int cols_{10};
